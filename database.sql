698bf80999f528be7eed75ffb510a3ae91f462f2
Michael Kourlas <michael@kourlas.net>
2016-10-01 00:04:33 -0400

    Remove database.sql

--- database.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `main` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Did` varchar(10) NOT NULL,
-  `RegistrationId` varchar(4096) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
